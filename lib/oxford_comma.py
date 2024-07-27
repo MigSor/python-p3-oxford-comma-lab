5ad6471ea016ab13f56fa50c26d5ad4aa4e3a0e8
Miguel Soriano <miguel.sor.88@gmail.com>
2024-07-27 19:46:13 +1000

    learned the join method for list and slicing with string methods

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,11 @@
 def oxford_comma(items):
-    return None
+
+    for item in items:
+        if len(items) > 2:
+            string_list = ", ".join(items[:-1]) + ", and " + str(items[-1])
+            return string_list
+        elif len(items) > 1:
+            string_list = " and ".join(items)
+            return string_list
+        else:
+            return item
